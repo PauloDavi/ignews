4a4ba138faea1ed940aaaabdfd1a44620de370a1
PauloDavi <paulo.araujo@cear.ufpb.br>
2021-07-21 11:01:04 -0300

    interation with faunaDb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,22 +1,25 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export function SignInButton() {
-  const isUserLogged = true;
+  const [ session ] = useSession()
 
-  return isUserLogged ? (
+  return session ? (
     <button
       type="button"
+      onClick={() => signOut()}
       className={styles.singInButton}
     >
       <FaGithub color="#04d361"/>
-      Paulo Davi
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button
       type="button"
+      onClick={() => signIn('github')}
       className={styles.singInButton}
     >
       <FaGithub color="#eba517"/>
